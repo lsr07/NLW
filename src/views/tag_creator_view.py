3f23f491cc96deaf6ea407fe179c66b5c484e6ad
Lucas R. <lcks250@gmail.com>
2024-02-11 19:06:34 -0300

    Feat: Implementig Error

--- src/views/tag_creator_view.py
+++ src/views/tag_creator_view.py
@@ -1,14 +1,24 @@
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_response import HttpResponse
+from src.controllers.tag_creator_controller import CreatorTagController
+
 
 class TagCreatorView:
+    '''
+    Responsável por interagir com HTTP
+    '''
      
     def validate_and_create(self, http_request: HttpRequest) -> HttpResponse:
+        tag_creator_controller = CreatorTagController()
+        
+        
         body = http_request.body
         product_code = body["product_code"]
    
         # Lógica de regra de negócio
-        print ('Estamos na view')
+        formatted_response = tag_creator_controller.create(product_code)
+        
+        
         # Retorno HTTP
-        return HttpResponse(status_code=200, body={"AAAAH": "É O DIABO"})
+        return HttpResponse(status_code=200, body=formatted_response)
     
